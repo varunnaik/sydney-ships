2f2eca14aafe0329f0cf77c40203613aee7081b5
Varun Naik <varun@vnaik.com>
2020-03-27 09:43:30 +1100

    Scroll to day

--- src/CaptureContainer/CaptureContainer.js
+++ src/CaptureContainer/CaptureContainer.js
@@ -1,36 +1,43 @@
 import React, { useLayoutEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import stringSimilarity from 'string-similarity';
 import CaptureRow from '../CaptureRow';
+import { DAYS_PER_PAGE } from '../constants';
 
 const CaptureContainerContainer = styled.div`
   box-sizing: border-box;
 `;
 
-const CaptureItemTitle = styled.h6`
-  font-weight: bold;
-  width: 1em;
-  text-orientation: sideways;
-  box-sizing: border-box;
-  border-right: 2px solid #88cccc;
-
-  @media screen and (max-width: 600px) {
-    text-orientation: normal;
-    border-right: 0;
-    border-top: 2px solid #88cccc;
-  }
-`;
-
-const getVisibleDays = (days, position = null) => {
+const getDaysToRender = (allDays, renderedDays, scrollToDay = null) => {
   // Read last scroll position from URL
   // If none then scroll to second page
   // Return new page of items
   // Update URL
-  return days.slice(0, 10);
+
+  if (isDayRendered(scrollToDay, renderedDays)) {
+    console.log('YES IT IS RENDERED');
+    return [...renderedDays];
+  }
+
+  return [...allDays.slice(0, 10)];
 };
 
-const getScrollPosition = () => {
-  const day = window.location.hash.match(/day=([^&]*)/);
-  return day && day.pop();
+const isDayRendered = (givenDay, renderedDays) =>
+  renderedDays && renderedDays.indexOf(givenDay) > -1;
+
+const getScrollPosition = allDays => {
+  // Always returns a date in the list of allDays, even if the day entered in the URL is not present in that list
+  const match = window.location.hash.match(/day=([^&]*)/);
+  const day = match && match.pop();
+
+  // Find closest day and return it
+  if (!allDays.length || allDays.indexOf(day) > -1) {
+    return day;
+  }
+
+  // As we store ISO dates a string compare works fine
+  const bestMatch = stringSimilarity.findBestMatch(day, allDays);
+  return allDays[bestMatch.bestMatchIndex];
 };
 
 export const CaptureContainer = ({ rows, shipInfo }) => {
@@ -38,26 +45,38 @@ export const CaptureContainer = ({ rows, shipInfo }) => {
     .sort()
     .reverse();
 
-  const daysToDisplay = getVisibleDays(allDays);
-  const [scrollToDay, setScrollToDay] = useState(getScrollPosition());
+  const renderedDays = getDaysToRender(allDays);
+  const [scrollToDay, setScrollToDay] = useState(getScrollPosition(allDays));
 
+  // Logic to scroll to the given day in URL
   const currentDay = useRef(null);
   useLayoutEffect(() => {
-    if (scrollToDay && currentDay.current) {
-      currentDay.current.scrollIntoView();
-      window.scrollTo(0, currentDay.current.offsetTop);
-      // If user types in new day scroll to it immediately
-      window.addEventListener('hashchange', () => setScrollToDay(getScrollPosition));
+    if (scrollToDay && allDays.length) {
+      if (!isDayRendered(scrollToDay, renderedDays)) {
+        // Render the day
+        //console.log('We need to render');
+        //setDaysToRender(getVisibleDays(allDays, daysToRender, scrollToDay));
+      }
 
-      return () => {
-        window.removeEventListener('hashchange', () => setScrollToDay(getScrollPosition));
-      };
+      if (currentDay.current) {
+        // This is only set if the element is rendered on the page
+
+        currentDay.current.scrollIntoView();
+      }
     }
+    // If user types in new day scroll to it immediately
+    window.addEventListener('hashchange', () => setScrollToDay(() => getScrollPosition(allDays)));
+
+    return () => {
+      window.removeEventListener('hashchange', () =>
+        setScrollToDay(() => getScrollPosition(allDays))
+      );
+    };
   });
 
   return (
     <CaptureContainerContainer>
-      {daysToDisplay.map(day => (
+      {renderedDays.map(day => (
         <CaptureRow
           scrollIntoViewRef={scrollToDay === day ? currentDay : undefined} // React does not like functional components using ref, so we have to call it something else here
           title={day}
