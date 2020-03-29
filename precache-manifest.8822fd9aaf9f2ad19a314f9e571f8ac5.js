02720bba89fd7e0650c3e9c9c585b98f65ba8e9d
Varun Naik <varun@vnaik.com>
2020-03-29 13:08:55 +1100

    Updates

---
+++ precache-manifest.8822fd9aaf9f2ad19a314f9e571f8ac5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5956136168e9d8659d0167882039265f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "677a3b66230862f742ce",
+    "url": "/static/css/2.00edd23d.chunk.css"
+  },
+  {
+    "revision": "d30ad00d8fdfc277a46a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "677a3b66230862f742ce",
+    "url": "/static/js/2.afe50666.chunk.js"
+  },
+  {
+    "revision": "8f99d3c045236db16e8a0a54f8548158",
+    "url": "/static/js/2.afe50666.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d30ad00d8fdfc277a46a",
+    "url": "/static/js/main.2725de7d.chunk.js"
+  },
+  {
+    "revision": "63d86dd818bd43f17307",
+    "url": "/static/js/runtime-main.3b68a519.js"
+  }
+]);
